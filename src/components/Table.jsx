48cf0b3be7d3fa9d85d5f5b1bb9b901d02db4b1d
Yossi Karasik <yosikari@gmail.com>
2023-06-27 14:33:08 +0300

    Add sort functionality

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,10 +1,41 @@
+import React, { useState, useMemo } from 'react'
+
+import { FaSort } from 'react-icons/fa'
+
 import TableCell from './TableCell'
 
-function Table(
-  { columns,
-    tableColumnVisibility,
-    rows }
-) {
+function Table({ columns, tableColumnVisibility, rows }) {
+  const [sortParams, setSortParams] = useState({ order: 'asc', colId: null })
+
+  const sortedRows = useMemo(() => {
+    const { order, colId } = sortParams
+
+    if (!Array.isArray(rows)) {
+      return []
+    }
+
+    return [...rows].sort((a, b) => {
+      if (a[colId] < b[colId]) {
+        return order === 'asc' ? -1 : 1
+      } else if (a[colId] > b[colId]) {
+        return order === 'asc' ? 1 : -1
+      } else {
+        return 0
+      }
+    })
+  }, [rows, sortParams])
+
+  const handleSort = (colId) => {
+    if (sortParams.colId === colId) {
+      setSortParams((prevSortParams) => ({
+        ...prevSortParams,
+        order: prevSortParams.order === 'asc' ? 'desc' : 'asc',
+      }))
+    } else {
+      setSortParams({ order: 'asc', colId })
+    }
+  }
+
   return columns ? (
     <div className="container table-container">
       <div className="scroll-container">
@@ -20,13 +51,21 @@ function Table(
             <tr>
               {columns.map((column) =>
                 tableColumnVisibility[column.ordinalNo] && (
-                  <th key={column.id}>{column.title}</th>
+                  <th key={column.id}>
+                    <span 
+                    title= {`Sort by ${column.id}`}
+                    onClick={() => handleSort(column.id)}
+                    className="column-title"
+                    >
+                      {column.title}<FaSort />
+                      </span>
+                  </th>
                 )
               )}
             </tr>
           </thead>
           <tbody>
-            {rows.map((row) => (
+            {sortedRows.map((row) => (
               <tr key={row.id}>
                 {columns.map((column) =>
                   tableColumnVisibility[column.ordinalNo] && (
